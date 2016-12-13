02e554a7f833246066061b2fa96413398d697f3d
Yoan Blanc <yoan@dosimple.ch>
2016-12-13 16:45:04 +0000

    Testing the product.
    As in Laravel, create is new + save. Adding the exclamation mark says
    that we want it to fail loudly by raising an exception.
    
    Signed-off-by: Yoan Blanc <yoan@dosimple.ch>

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -5,4 +5,24 @@ class ProductTest < ActiveSupport::TestCase
     product = Product.find_by(title: 'T-shirt')
     assert 0 < product.price
   end
+
+  test 'with a title, description and price' do
+    prod = Product.create(title: 'Title', description: 'Lorem', price: 0.1 + 0.2)
+
+    assert prod.valid?
+    assert prod.id
+    assert 'Title' == prod.title
+    assert 'Lorem' == prod.description
+    assert 0.3 == prod.price
+  end
+
+  test 'must have a title' do
+    assert_not Product.create(price: 10).valid?
+  end
+
+  test 'must have a price greater than zero' do
+    assert_raise do
+      Product.create!(title: 'Untitled', price: 0)
+    end
+  end
 end
