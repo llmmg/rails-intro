57c15b734d06dac75039a3e86489d3b1bf8eb4db
Yoan Blanc <yoan@dosimple.ch>
2016-12-13 16:53:20 +0000

    Adding a smoke test for the ProductsController.
        $ rails test
    
    A specific gem is required to run such tests. Add it under the
    :test group.
    
    Signed-off-by: Yoan Blanc <yoan@dosimple.ch>

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,9 +1,12 @@
 require 'test_helper'
 
 class ProductsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get products_index_url
-    assert_response :success
-  end
+  test 'should get products on /' do
+    get '/'
+    # or:
+    #get root_url
 
+    assert_response :success
+    assert_not_nil assigns(:products)
+  end
 end
