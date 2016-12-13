e35e3b08299f8265aba494c7ea4620f1b06908bb
Yoan Blanc <yoan@dosimple.ch>
2016-12-13 17:33:31 +0000

    Adding has_attached_file and validations.
    Signed-off-by: Yoan Blanc <yoan@dosimple.ch>

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,8 @@ class Product < ApplicationRecord
   validates :price, numericality: { greater_than: 0 }
 
   has_and_belongs_to_many :sizes, uniq: true
+
+  has_attached_file :image
+  validates_attachment_content_type :image, content_type: /\Aimage/
+  validates_attachment_file_name :image, matches: [/png\z/, /jpe?g\z/]
 end
