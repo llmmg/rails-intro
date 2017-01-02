3ad3dcfd345b5d97fe8a31da0c7098762777a647
Yoan Blanc <yoan@dosimple.ch>
2017-01-02 19:30:28 +0100

    Setup of rails_admin.
        $ rails g rails_admin:install
    
    Signed-off-by: Yoan Blanc <yoan@dosimple.ch>

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+
   root 'products#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
