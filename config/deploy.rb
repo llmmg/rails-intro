aa3830268feb7a07a25fe787db011f5a280da96c
lancelot <lancelotlancelot94@gmail.com>
2017-03-15 15:13:31 +0100

    update

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "3.8.0"
 
 set :application, "rails-intro"
-set :repo_url, "git@github.com/llmmg/rails-intro.git"
+set :repo_url, "https://github.com/llmmg/rails-intro.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
